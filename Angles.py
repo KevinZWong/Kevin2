f3c9ccbf0d33fe04f2b1932cf0ea525af1f6d999
KevinZWong <kevinzw8@gmail.com>
2020-01-20 13:35:55 -0800

    merge trial 1

---
+++ Angles.py
@@ -0,0 +1,23 @@
+def angles():
+    list1Trig = ['opposite', 'hypotenuse', 'adjacent']
+    list2 = []
+    hypo = 'No'
+    oppo = 'No'
+    adj = 'No'
+    for i in list1:
+        print('Please enter:', i) 
+                oppo = 'yes'
+        elif i == 'adjacent':
+            if var1 == 0:
+                list2.remove(var1)
+            elTrig== 'No' and oppo = 'No' and adj = 'No':
+        print('Nothing inputed')
+
+
+
+
+
+
+
+
+
--- ModifiedPT.py
+++ ModifiedPT.py
@@ -1,26 +1,33 @@
-def Trig():
+def Pythagram():
     import math
-    list1 = ['opposite', 'hypotenuse', 'adjacent']
+    list1Trig = ['opposite', 'hypotenuse', 'adjacent']
     list2 = []
     hypo = 'No'
     oppo = 'No'
     adj = 'No'
     for i in list1:
-        print('Please enter:', i)
-        var1 = int(input())
-        list2.append(var1)
-        if var1 == 0:
-            list2.remove(var1)
-        elif i == 'hypotenuse':
-            if var1 == 0:
-                list2.remove(var1)
-            else:
-                hypo = 'yes'
-        elif i == 'opposite':
-            if var1 == 0:
-                list2.remove(var1)
-            else:
+        print('Please enter:', i) 
                 oppo = 'yes'
+        elif i == 'adjacent':
+            if var1 == 0:
+                list2.remove(var1)
+            elTrig== 'No' and oppo = 'No' and adj = 'No':
+        print('Nothing inputed')
+
+    if hypo == 'No' and oppo = 'No':
+        print('Not enough inputed')
+    
+    if adj == 'No' and oppo = 'No':
+        print('Not enough inputed')
+
+    if hypo == 'No' and adj = 'No':
+        print('Not enough inputed')
+
+    if hypo == 'yes' and oppo = 'yes' and adj = 'yes':
+        print('Opposite:', list2[0])
+        print('Adjacent:', list2[2])
+        print('Hypotenuse:', list2[1])
+
     
 
     if hypo == 'No':
@@ -30,15 +37,11 @@ def Trig():
         print('Hypotenuse:', answer)
         print('Hypotenuse:', answer)
     elif hypo == 'yes' and oppo == 'No':
-        answer = math.sqrt(list2[0]*list2[0] - list2[1]*list2[1])
-        print('Opposite:', answer)
-        print('Adjacent:', list2[1])
-        print('Hypotenuse:', list2[0])
-    elif hypo == 'yes' and oppo == 'yes':
-        answer = math.sqrt(list2[1]*list2[1] - list2[0]*list2[0])
+        answeTriger = math.sqrt(list2[1]*list2[1] - list2[0]*list2[0])
         print('Opposite:', list2[0])
         print('Adjacent:', answer)
         print('Hypotenuse:', list2[1])
+    else:
+        print('Error Please Try Again.')
 
-
-Trig()
+Pythagram()
